fe9bc01f844e96120a50cae5662e466baf878e02
Andréas Kühne <andreas@kuhne.se>
2018-07-31 11:54:28 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="apex_auth",
-    version="0.1.6",
+    version="0.1.7",
     author="Andréas Kühne",
     author_email="andreas.kuhne@promoteint.com",
     description="",
