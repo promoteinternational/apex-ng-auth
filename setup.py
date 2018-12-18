9d28b0119428bda2f190b95b15af75bfcba93ef9
arturveres <artur8118@gmail.com>
2018-12-18 17:11:46 +0200

    Fixing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="apex_auth",
-    version="0.1.7",
+    version="0.1.8",
     author="Andréas Kühne",
     author_email="andreas.kuhne@promoteint.com",
     description="",
